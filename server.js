2e1d40b1252bdb9750a43c563e351a471ec05f66
Rishabh Gist <damyrachit@gmail.com>
2022-10-24 15:54:35 +0530

    added home get request

--- server.js
+++ server.js
@@ -2,4 +2,10 @@
 import express from "express";
 const app = express();
 
+// Starting express server 
+app.listen(3000);
 
+// GET Requests 
+app.get("/", (res, req) => {
+    req.send("Hello World");
+});
