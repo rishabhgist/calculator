2a7d4dc6257d0f4cf3ad66cf96df6d3a0f818e59
Rishabh Gist <damyrachit@gmail.com>
2022-10-24 16:15:51 +0530

    added fileURLToPath and dirname

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-//jshint esversion:6
+//jshint esversion:11
+
 import express from "express";
-const app = express();
+import { fileURLToPath } from "url";
+import { dirname } from "path"; 
+
+const app = express(); // Initialize express
+const __filename = fileURLToPath(import.meta.url); // Initialize fileURLToPath
+const __dirname = dirname(__filename); // Initialize dirname
 
 // Starting express server 
 app.listen(3000);
 
 // GET Requests 
 app.get("/", (res, req) => {
-    req.send("Hello World");
+    console.log(__dirname);
+    //req.sendFile(__filename + "/index.html");
 });
